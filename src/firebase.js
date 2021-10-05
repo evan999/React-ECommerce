3549de464b658330aa487e03b9b7224df8be39bf
evan999 <emeshberge@gmail.com>
2021-10-04 23:03:18 -0400

    Build header component. Install dependencies. App in runnable state.

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCgkdMfns0mbml8-ZXMvVk2i_hw7Ro8dUg",
+	authDomain: "ecommerce-app-cd534.firebaseapp.com",
+	projectId: "ecommerce-app-cd534",
+	storageBucket: "ecommerce-app-cd534.appspot.com",
+	messagingSenderId: "284438796919",
+	appId: "1:284438796919:web:7519a0f3da505057404472",
+	measurementId: "G-3FP7FG0GRS"
+};
